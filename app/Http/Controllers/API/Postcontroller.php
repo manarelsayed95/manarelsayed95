1eaa3025ebfab69e9604fe2150a13fddf7fe687f
manarelsayed95 <manarelsayed969@gmail.com>
2020-04-03 23:01:01 +0200

    store api

--- app/Http/Controllers/API/Postcontroller.php
+++ app/Http/Controllers/API/Postcontroller.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 use App\Post;
 use App\User;
 
 use App\Http\Resources\PostResource;
+use Illuminate\Validation\ValidationException;
 
 class Postcontroller extends Controller
 {
@@ -26,9 +28,38 @@ class Postcontroller extends Controller
     }
 
     public function show($post){
-        return  Post::find($post) ?
-            new PostResource(
+        return  Post::find($post)
+           ?new PostResource(
                 Post::find($post)
             ) : 'not exist';
     }
+    public function store(Request $request)
+    {
+
+        // $input = $request->all();
+
+        $request->validate([
+            'title' => 'required|min:3',
+            'description' => 'required|min:5',
+            'user_name' => 'required'
+        ]);
+
+        // if($validator->fails()){
+        //     return $this->sendError('Validation Error.', $validator->errors());       
+        // }
+        $username=$request->user_name;
+        // dd($username);
+        $userData= DB::table('users')->where('name',$username)->get();
+        // dd($userData[0]->id);
+
+       $post= Post::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' =>   $userData[0]->id,
+        ]);
+        return new PostResource($post);
+        // return $this->sendResponse($post->toArray(), 'Post created successfully.');
+
+    }
+    
 }
--- routes/api.php
+++ routes/api.php
@@ -21,8 +21,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//index view  "have table of all posts"
 Route::get('/Posts','API\Postcontroller@index')->middleware('auth:sanctum');
+//store view to  get data from form then store it into database 
+Route::post('/Posts/store', 'API\Postcontroller@store')->middleware('auth:sanctum');
+//show view "have all details about one post"
 Route::get('/Posts/{post}','API\Postcontroller@show')->middleware('auth:sanctum');
+
 
 Route::post('/sanctum/token', function (Request $request) {
     $request->validate([
